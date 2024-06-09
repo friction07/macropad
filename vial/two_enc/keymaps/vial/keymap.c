9f763f3ad5a11610b6275042214916a372c2d8a0
friction07 <rahiljain2197@gmail.com>
2024-06-09 22:15:22 +0530

    zmk

--- vial/two_enc/keymaps/vial/keymap.c
+++ vial/two_enc/keymaps/vial/keymap.c
@@ -3,10 +3,28 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    KB_NAME = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case KB_NAME:
+        if (record->event.pressed) {
+            // when keycode KB_NAME is pressed
+            SEND_STRING("Thermal Pad");
+        } else {
+            // when keycode KB_NAME is released
+        }
+        break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,
-        KC_PSCR, KC_HOME, RGB_MOD,
+        KC_PSCR, KC_HOME, KB_NAME,
         KC_LEFT, KC_END,  KC_RGHT, KC_SLEP
     ),
 };
