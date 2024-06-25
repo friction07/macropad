9fc8a2bb2fa71b249a27647a7ac2de1ed4311203
friction07 <rahiljain2197@gmail.com>
2024-06-25 22:50:45 +0530

    fixes

--- vial/oled_enc/keymaps/vial/rules.mk
+++ vial/oled_enc/keymaps/vial/rules.mk
@@ -1,3 +1,4 @@
 ENCODER_MAP_ENABLE = yes
+OLED_ENABLE = yes
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
--- vial/two_enc/keymaps/vial/keymap.c
+++ vial/two_enc/keymaps/vial/keymap.c
@@ -31,6 +31,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MS_DOWN, KC_MS_UP) }
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) }
 };
 #endif
